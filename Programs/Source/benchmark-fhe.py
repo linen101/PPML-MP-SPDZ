d830e4db7e6a8a775944614d5988a80ad30980e3
linen101 <linen@protonmail.com>
2025-07-28 13:00:46 +0300

    bench fhe

--- Programs/Source/benchmark-fhe.py
+++ Programs/Source/benchmark-fhe.py
@@ -4,7 +4,7 @@ program.set_security(40)
 
 # set the bit length of the cleartext for the comparisons
 #program.set_bit_length(64)
-#program.set_bit_length(240)
+program.set_bit_length(240)
 # The length 106 is composed as follows: 
 # assuming 64-bit integers, the difference used for comparison is a 65-bit integer, to which 40 bits are added for statistical masking, resulting in a 105 bits, 
 # and it takes a 106-bit prime to able to contain all 105-bit numbers. Finally, the last line indicates which compile-time options would change the program.
@@ -34,10 +34,10 @@ d1 = 100
 d2 = 200 
 d3 = 400
 d4 = 800
-d5 = 0
+#d5 = 0
 d5 = 1600
-d6 = 0
-#d6 = 3200
+#d6 = 0
+d6 = 3200
 
 n_threads = 48
 

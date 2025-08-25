16ab9bbca162373df5a175d0616d8801c9277932
linen101 <linen@protonmail.com>
2025-08-25 09:59:36 +0300

    corrections

--- Programs/Source/bench_trip_mpc_proofs_rounds.py
+++ Programs/Source/bench_trip_mpc_proofs_rounds.py
@@ -35,7 +35,7 @@ if len(sys.argv) > 4 and sys.argv[4].isdigit():
 alpha = 0
 beta = 1 
 B = 10
-initial_input_commitment_proofs(n, d, alpha, beta, m, B) 
-#model_input_commitment_proofs(n, d, alpha, beta , m)
-#participation_set_update_proofs(n, m, alpha, beta)
+#initial_input_commitment_proofs(n, d, alpha, beta, m, B) 
+model_input_commitment_proofs(n, d, alpha, beta , m)
+participation_set_update_proofs(n, m, alpha, beta)
 

d10aa661e31d34870981806565edf88df979ff92
linen101 <linen@protonmail.com>
2025-08-25 11:37:26 +0300

    only model input

--- Programs/Source/bench_trip_mpc_proofs_rounds.py
+++ Programs/Source/bench_trip_mpc_proofs_rounds.py
@@ -37,5 +37,5 @@ beta = 1
 B = 10
 #initial_input_commitment_proofs(n, d, alpha, beta, m, B) 
 model_input_commitment_proofs(n, d, alpha, beta , m)
-participation_set_update_proofs(n, m, alpha, beta)
+#participation_set_update_proofs(n, m, alpha, beta)
 
